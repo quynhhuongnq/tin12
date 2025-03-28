2a4c81dbb4fe8af8e37d40bcd033a4293c17a30f
quynhhuongnq <quynhhuon2@gmail.com>
2025-03-28 12:32:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function login()
 { 
 let u = username.value
 let p = pw.value
- ì (u=="hieu" && p=="123") 
+if (u=="hieu" && p=="123") 
 { 
   window.open ("home.html") 
+document.getElementById(compiler).style.display="block";
+}
+  else 
+      document.write("Không hợp lệ")
